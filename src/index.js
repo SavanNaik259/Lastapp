4e8a9c07b842a0aa1c061ca09db98145640c672a
Savan <githost.2007@gmail.com>
2024-07-25 00:38:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
